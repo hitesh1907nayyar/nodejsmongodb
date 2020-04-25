7d59e3941bbb49850124718dad3c840fa7e5c772
Chetna Aggarwal <chetnaaggarwal@Chetnas-MacBook-Pro.local>
2020-04-25 13:03:01 +0530

    All

--- config/database.js
+++ config/database.js
@@ -1,4 +1,4 @@
 module.exports = {
-    remoteUrl : 'mongodb://vaibhav:DrXTMzTQtx9s80C1cLOaRRG6qLX5Nk2QB8A3l144Rc51ZVQz6Sa3fQMbsQtzKJv5f3Z4didABCT4Rwat59N6uw==@vaibhav.mongo.cosmos.azure.com:10255/?ssl=true&replicaSet=globaldb&retrywrites=false&maxIdleTimeMS=120000&appName=@vaibhav@',    
+    remoteUrl : 'mongodb://vaibhav:UbkYeIpZOEjwQ3PVPCYhmWRLBoAvhS495z8UHafnYo1IGHVHMzWPh68nujETEqnJNdz9tlczXU2ZFC1PToZhaQ==@vaibhav.mongo.cosmos.azure.com:10255/?ssl=true&replicaSet=globaldb&retrywrites=false&maxIdleTimeMS=120000&appName=@vaibhav@',    
     localUrl: 'mongodb://localhost/meanstacktutorials'
 };
